f92e94efbaed74f355930fff8aada5772c7be0a4
Bruno Licuadri <blicuadri@gmail.com>
2022-02-10 12:00:52 -0300

    Starting to implement Board and Piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,11 +5,13 @@ public class Board {
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	public Board(int rows, Piece[][] pieces) {
+	
+	public Board(int rows, int columns) {
 		this.rows = rows;
-		this.pieces = pieces;
+		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
+	
 	public int getRows() {
 		return rows;
 	}
@@ -21,6 +23,13 @@ public class Board {
 	}
 	public void setColumns(int columns) {
 		this.columns = columns;
+	}
+	
+	public Piece piece (int row, int column) {
+		return pieces[row][column];
+	}
+	public Piece piece (Position position) {
+		return pieces[position.getRow()][position.getColumn()];
 	}
 
 }
